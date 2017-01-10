9d400c6b9779052972edc1d2f862c3e961070b25
yilai <yi_lai@outlook.com>
2017-01-10 19:39:16 +0000

    This is the third sample

--- answers.js
+++ answers.js
@@ -5,3 +5,8 @@ console.log(name[0]);
 //Write a function that takes a string and returns the last character of a string.
 var name = "orange";
 console.log(name[5]);
+
+//Write a function that takes a string and a number, and returns the character at the position represented by the number.
+var things = ["line",10];
+console.log (things[1]);
+
